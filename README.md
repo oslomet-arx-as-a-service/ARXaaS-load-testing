21d1df015358de41e0c570f4b2e808369917ebda
sondre <sondre.hal@gmail.com>
2019-04-09 23:41:12 +0200

    CREATE locust test for load testing

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # ARXaaS load testing
 
 Contains scripts and notebooks for load testing and analyzing load handling of ARXaaS
+
+
+### Locust tests
+
+Example run
+```bash
+python -m locust.main -f scripts/analyze_locust_test.py --host http://localhost:8080
+```
---
+++ scripts/analyze_locust_test.py
@@ -0,0 +1,25 @@
+from locust import HttpLocust, TaskSet, task
+from scripts.data import test_data
+from pyaaas.models.request_builder import RequestBuilder
+
+
+def analyze_request_content():
+    dataset = test_data.dummy_dataset_double_n_times(10)
+    request = RequestBuilder(dataset).build_analyze_request()
+    return request
+
+
+request = analyze_request_content()
+
+
+class UserBehavior(TaskSet):
+
+    @task(1)
+    def analyze(self):
+        self.client.post("/api/analyze", json=request)
+
+
+class WebsiteUser(HttpLocust):
+    task_set = UserBehavior
+    min_wait = 1000
+    max_wait = 1000
